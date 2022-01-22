c94683bac76e85f8f8b48231e90e97f6235e1f45
mpisters <michelle.pisters@infi.nl>
2022-01-22 15:29:23 +0100

    add size to beverage class

--- src/Starbuzz/Beverage.java
+++ src/Starbuzz/Beverage.java
@@ -1,10 +1,20 @@
 package Starbuzz;
 
 public abstract class Beverage {
+    public enum Size {Tall, Grande, Vent};
+    Size size = Size.Tall;
     String description = "Unknown beverage";
 
     public String getDescription(){
         return description;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
     }
 
     public abstract double cost();
